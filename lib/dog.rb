b2005ec04b258958af3b68a1a720bcd4755e2016
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-02-17 07:43:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,16 @@
 # Your code goes here!
 class Dog
   
-  def name
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
   
 end
+
+fido = Dog.new
+fido.name = "Fido"
+
